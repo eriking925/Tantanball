2216b70a022f42a719cbb278fe4df1a0d557a2f1
Eric <gugokh@gmail.com>
2017-12-02 00:03:31 +0800

    Merge branch 'patch-1' of https://github.com/eriking925/Tantanball
    # Conflicts:
    #	app/src/main/java/game/tantanball/MainActivity.java

--- app/src/main/java/game/tantanball/MainActivity.java
+++ app/src/main/java/game/tantanball/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //testingabc
+        //testingabcasdfadw
         setContentView(R.layout.activity_main);
 
     }
